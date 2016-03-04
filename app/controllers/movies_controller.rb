496ef8c35abe28fb5f6a3075246b551019d59a3e
amanbhal <amandeep.bhal92@gmail.com>
2016-03-03 19:30:45 -0600

    Changes made in Controller/View/Model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,35 +11,82 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
-  end
+        @allRatingType = Movie.all_ratings
 
+        if(params[:sort] != nil and params[:sort] != session[:sort])
+          session[:sort] = params[:sort]
+        end
+        if(params[:ratings] != nil and params[:ratings] != session[:ratings])
+        session[:ratings] = params[:ratings]
+        end
+
+        session_params = { :sort => session[:sort], :ratings => session[:ratings] }
+        param_params = { :sort => params[:sort], :ratings => params[:ratings] }
+
+        if(session_params != param_params)
+          flash.keep
+          redirect_to(session_params)
+        end
+
+
+        if(session[:ratings] != nil)
+          @sel_ratings =session[:ratings].keys
+        else
+          @sel_ratings =Movie.all_ratings
+        end
+
+        if(session[:sort]=='title')
+          if (session[:ratings] )
+            @movies = Movie.where("rating in (?)", @sel_ratings).order('title')
+          else
+            @movies = Movie.all.order('title')
+            end
+        elsif(session[:sort]=='release_date')
+          if (session[:ratings])
+            @movies = Movie.where("rating in (?)", @sel_ratings).order('release_date')
+             else
+            @movies = Movie.all.order('release_date')
+          end
+        else
+          if (session[:ratings] )
+            @movies = Movie.where("rating in (?)", @sel_ratings)
+            else
+          @movies = Movie.all
+            end
+        end
+        @title_header = params[:sort]=='title' ?'hilite':nil
+        @release_date_header = params[:sort]=='release_date' ?'hilite':nil
+  end
   def new
     # default: render 'new' template
   end
-
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
-
   def edit
     @movie = Movie.find params[:id]
   end
-
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
-
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+
+  #private
+  #def get_array(priority_array, stored_array)
+  #  if !priority_array.nil? and stored_array != priority_array
+  #    stored_array = priority_array
+  #  end
+  #  return stored_array
+  #end
 
 end
